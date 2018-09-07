3574f1accd7a3453860336c2b0234faee2934c31
germankoco <gmtz.1991@gmail.com>
2018-09-06 23:08:48 -0500

    new project

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyB6uPna2K99PM7aiYdlDiFzTzj1LDnulpI",
-  authDomain: "reactexample-4a692.firebaseapp.com",
-  databaseURL: "https://reactexample-4a692.firebaseio.com",
-  projectId: "reactexample-4a692",
-  storageBucket: "reactexample-4a692.appspot.com",
-  messagingSenderId: "943468013217"
-};
+    apiKey: "AIzaSyB6uPna2K99PM7aiYdlDiFzTzj1LDnulpI",
+    authDomain: "reactexample-4a692.firebaseapp.com",
+    databaseURL: "https://reactexample-4a692.firebaseio.com",
+    projectId: "reactexample-4a692",
+    storageBucket: "reactexample-4a692.appspot.com",
+    messagingSenderId: "943468013217"
+  };
 firebase.initializeApp(config); 
 
 export const firebaseAuth = firebase.auth();
